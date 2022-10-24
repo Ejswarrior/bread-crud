0e4784d27e810e08bc9aa553a902da81c68e906f
Erik Sabin <eriksabin97@gmail.com>
2022-10-24 18:44:51 -0400

    edit and update routes complete

--- controllers/bread_controller.js
+++ controllers/bread_controller.js
@@ -17,6 +17,25 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('Edit', {
+    bread: bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
+
+
 breads.get('/:arrayIndex', (req, res) => {
     if(bread[req.params.arrayIndex]){
         res.render('Show',{
@@ -48,6 +67,10 @@ breads.delete('/:indexArray', (req, res) => {
   bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
 })
+
+// EDIT
+
+
 
 
 
--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Show ({bread, index}) {
+function Show ({bread, index, ingIndex}) {
 
       return (
         <Default>
@@ -16,10 +16,14 @@ function Show ({bread, index}) {
           have gluten.
         </p>
         <img src={bread.image} alt={bread.name} />
+        <li>{bread.ingrediants}</li>
         <li><a href="/breads">Go home</a></li>
         <form action={"/breads/index?_method=DELETE"} method="post">
           <input type='submit' value='Delete'></input>
         </form>
+
+        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
       </Default>
       )
 }
